a7b009a88bb164e29d394e7b03f5413ce64c797d
lochinbek9 <lochinbeksetor@gmail.com>
2024-12-16 01:48:41 +0300

    error fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,17 @@
 //  PRELOAD
 
 const preloader = document.querySelector(".preload");
+const menuBtn = document.querySelector(".nav-open-btn");
+const navbar = document.querySelector(".navbar")
+
+
+
 
 window.addEventListener("load", function(){
     preloader.classList.add("loaded");
     document.body.classList.add("loaded");
 });
+
+menuBtn.addEventListener('click', (e) => {
+    navbar.classList.toggle('active');
+})
