0a496109ccdf20f2ef95bef486cb7d76d5658541
lochinbek9 <lochinbeksetor@gmail.com>
2024-12-16 13:39:11 +0500

    topnavbar error fixed and menu toggle class active

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,9 +4,9 @@
 
 const preloader = document.querySelector(".preload");
 const menuBtn = document.querySelector(".nav-open-btn");
-const navbar = document.querySelector(".navbar")
+const navbar = document.querySelector(".navbar");
 
-
+const closeBtn = document.querySelector(".close-btn");
 
 
 window.addEventListener("load", function(){
@@ -16,4 +16,8 @@ window.addEventListener("load", function(){
 
 menuBtn.addEventListener('click', (e) => {
     navbar.classList.toggle('active');
-})
+});
+
+closeBtn.addEventListener('click', (e) => {
+    navbar.classList.toggle('active');
+});
